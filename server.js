2f2554e1d37486e62c748ab8e8cf4e7ef9a609cb
abhishek3100 <abhishekku3100.12@gmail.com>
2021-07-12 16:11:55 +0530

    after checking skaffold

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, How you doing, want some coffee n biscuits?');
+  res.send('Hello World, Hope you are doing well !!');
 });
 
 app.listen(PORT, HOST);
